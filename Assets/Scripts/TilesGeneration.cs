a0024366226eb42fed0672b0cdb4aa79568d69f6
Negar <neg_r.saeidi95@yahoo.com>
2022-03-14 00:43:34 -0500

    FadeIn and FadeOut animations added

--- Assets/Scripts/TilesGeneration.cs
+++ Assets/Scripts/TilesGeneration.cs
@@ -9,9 +9,12 @@ public class TilesGeneration : MonoBehaviour
     public GameObject[] RowsParent;
     public GameObject root;
     public Sprite[] tileSprites;
+    private int maxRange;
     public static  List<GameObject> gridTiles;
+    public bool FadeInCoroutineStarted, FadeOutCoroutineStarted;
     void Start()
     {
+        maxRange = 11;
         gridTiles = new List<GameObject>();
         for (int i=0; i<RowsParent.Length;i++)
         GenerateTiles(i);
@@ -23,7 +26,7 @@ public class TilesGeneration : MonoBehaviour
         for (int i = 0; i < 7; i++)
         {
             GameObject tile = Instantiate(tilePrefab, RowsParent[row].transform.position, RowsParent[row].transform.rotation, RowsParent[row].transform);
-            int rand = Random.Range(1, 10);
+            int rand = Random.Range(1, maxRange);
             tile.GetComponent<Image>().sprite = tileSprites[rand];
             gridTiles.Add(tile);
             tile.transform.parent = RowsParent[row].transform;
@@ -33,8 +36,8 @@ public class TilesGeneration : MonoBehaviour
     }
     private void Update()
     {
-        CheckVertical();
-        CheckHorizontal();
+       CheckVertical();
+       CheckHorizontal();
     }
     private void CheckVertical()
     {
@@ -44,10 +47,27 @@ public class TilesGeneration : MonoBehaviour
                 if (gridTiles[i].GetComponent<Image>().sprite == gridTiles[i + 7].GetComponent<Image>().sprite)
                 {
                     if (gridTiles[i].GetComponent<Image>().sprite == gridTiles[i + 14].GetComponent<Image>().sprite)
-                    {
-                        gridTiles[i].transform.GetChild(0).gameObject.SetActive(true);
-                        gridTiles[i+7].transform.GetChild(0).gameObject.SetActive(true);
-                        gridTiles[i+14].transform.GetChild(0).gameObject.SetActive(true);
+                    { 
+                        if (!FadeInCoroutineStarted )
+                        {
+                            if (!FadeOutCoroutineStarted)
+                            {
+                                FadeOutCoroutineStarted = true;
+                                gridTiles[i].GetComponent<Animator>().SetTrigger("Disappear");
+                                gridTiles[i+7].GetComponent<Animator>().SetTrigger("Disappear");
+                                gridTiles[i+14].GetComponent<Animator>().SetTrigger("Disappear");
+                                StartCoroutine(FadeOut(i, i + 7, i + 14));
+                            }
+                           
+                         
+                          
+
+
+                            FadeInCoroutineStarted = true;
+                            StartCoroutine(DelayBeforeRegenerate(i,i+7,i+14));
+                          
+                        }
+
                     }
                 }
         }
@@ -64,10 +84,20 @@ public class TilesGeneration : MonoBehaviour
                      if ((i / 7) == ((i + 2) / 7))
                     if (gridTiles[i].GetComponent<Image>().sprite == gridTiles[i + 2].GetComponent<Image>().sprite)
                     {
+                                if (!FadeInCoroutineStarted)
+                                {
+                                    if (!FadeOutCoroutineStarted)
+                                    {
+                                        FadeOutCoroutineStarted = true;
+                                        gridTiles[i].GetComponent<Animator>().SetTrigger("Disappear");
+                                        gridTiles[i + 1].GetComponent<Animator>().SetTrigger("Disappear");
+                                        gridTiles[i + 2].GetComponent<Animator>().SetTrigger("Disappear");
+                                        StartCoroutine(FadeOut(i, i + 1, i + 2));
+                                    }
+                                    FadeInCoroutineStarted = true;
+                                    StartCoroutine(DelayBeforeRegenerate(i, i + 1, i + 2));
 
-                        gridTiles[i].transform.GetChild(0).gameObject.SetActive(true);
-                        gridTiles[i + 1].transform.GetChild(0).gameObject.SetActive(true);
-                        gridTiles[i + 2].transform.GetChild(0).gameObject.SetActive(true);
+                                }
                     }
                 }
             }
@@ -81,4 +111,31 @@ public class TilesGeneration : MonoBehaviour
 
     }
 
+    IEnumerator FadeOut(int i, int j, int k)
+    {
+        yield return new WaitForSeconds(0.5f);
+      
+        gridTiles[i].GetComponent<Image>().color = new Color(gridTiles[i].GetComponent<Image>().color.r, gridTiles[i].GetComponent<Image>().color.g, gridTiles[i].GetComponent<Image>().color.b, 0);
+        gridTiles[j].GetComponent<Image>().color = new Color(gridTiles[j].GetComponent<Image>().color.r, gridTiles[j].GetComponent<Image>().color.g, gridTiles[j].GetComponent<Image>().color.b, 0);
+        gridTiles[k].GetComponent<Image>().color = new Color(gridTiles[k].GetComponent<Image>().color.r, gridTiles[k].GetComponent<Image>().color.g, gridTiles[k].GetComponent<Image>().color.b, 0);
+        FadeOutCoroutineStarted = false;
+    }
+    IEnumerator DelayBeforeRegenerate(int i,int j, int k)
+    {
+        yield return new WaitForSeconds(2f);
+
+        int rand = Random.Range(1, maxRange);
+        gridTiles[i].GetComponent<Image>().sprite = tileSprites[rand];
+       gridTiles[i].GetComponent<Animator>().SetTrigger("Appear");
+        gridTiles[i].GetComponent<Image>().color = new Color(gridTiles[i].GetComponent<Image>().color.r, gridTiles[i].GetComponent<Image>().color.g, gridTiles[i].GetComponent<Image>().color.b, 255);
+        rand = Random.Range(1, maxRange);
+        gridTiles[j].GetComponent<Image>().sprite = tileSprites[rand];
+        gridTiles[j].GetComponent<Animator>().SetTrigger("Appear");
+        gridTiles[j].GetComponent<Image>().color = new Color(gridTiles[j].GetComponent<Image>().color.r, gridTiles[j].GetComponent<Image>().color.g, gridTiles[j].GetComponent<Image>().color.b, 255);
+        rand = Random.Range(1, maxRange);
+        gridTiles[k].GetComponent<Image>().sprite = tileSprites[rand];
+       gridTiles[k].GetComponent<Animator>().SetTrigger("Appear");
+        gridTiles[k].GetComponent<Image>().color = new Color(gridTiles[k].GetComponent<Image>().color.r, gridTiles[k].GetComponent<Image>().color.g, gridTiles[k].GetComponent<Image>().color.b, 255);
+        FadeInCoroutineStarted = false;
+    }
 }
